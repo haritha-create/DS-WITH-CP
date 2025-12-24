39c5c2e7ab594982d6c5bf15773bb5d6ab289057
haritha-create <241801340001@cutmap.ac.in>
2025-12-24 14:15:35 +0530

    Add Student class with name and age attributes

---
+++ module1_Array/task3.java
@@ -0,0 +1,13 @@
+class Student
+{
+    String name;
+    int age;
+    public static void main(String arge[])
+    {
+        Student b=new Student();
+        b.name="haritha";
+        b.age=19;
+    System.out.println("name:"+b.name);
+    System.out.println("age:"+b.age);
+    }
+}
